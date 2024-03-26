ed5b70fc7fb19aa9f0a8b80dac04f74231d832a1
Shruti M S <54471978+shrutitaylor@users.noreply.github.com>
2024-03-26 18:40:57 +1100

    Soil website

---
+++ src/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="mt-auto py-3 bg-dark text-white" style={{fontFamily : "Raleway, sans-serif", fontWeight:200, fontSize:'15px'}}>
+      <div className="container">Basic Login and Logout Example with Functions and Hooks &copy; 2022</div>
+    </footer>
+  );
+}
+
+export default Footer;
