2129a479f29ed303abbe1f340010777318b671eb
shrutitaylor <shruti128770@gmail.com>
2024-03-28 23:15:12 +1100

    added fonts

--- src/fragments/Footer.js
+++ src/fragments/Footer.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="mt-auto py-3 bg-dark text-white" style={{fontFamily : "Raleway, sans-serif", fontWeight:200, fontSize:'15px'}}>
-      <div className="container">Basic Login and Logout Example with Functions and Hooks &copy; 2022</div>
+    <footer className="mt-auto py-3 bg-dark text-white" style={{ alignItems:'center',fontFamily: "Syne, sans-serif",  fontWeight: '400',fontStyle: "normal", fontSize:'15px'}}>
+      <div className="container">Soil &copy; 2022. All rights reserved.</div>
     </footer>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import logo from "../logo.svg";
 function Home(props) {
   return (
     <div className="text-center">
-      <h1 className="display-4">Home</h1>
+      {/* <h1 className="display-4">Home</h1> */}
       {props.username !== null && <h4><strong>Hello {props.username}!</strong></h4>}
       <img src={logo} className="w-50" alt="logo" />
     </div>
